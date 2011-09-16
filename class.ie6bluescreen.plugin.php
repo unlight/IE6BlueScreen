368bbfad708a5b95f2e6ad59e53b5769a5e2ec57
S <unlights@gmail.com>
2011-09-16 15:18:26 +0400

    bug: max() function for empty array

--- class.ie6bluescreen.plugin.php
+++ class.ie6bluescreen.plugin.php
@@ -1,9 +1,9 @@
 <?php if (!defined('APPLICATION')) exit();
 
 $PluginInfo['IE6BlueScreen'] = array(
-	'Name' => 'IE6BlueScreen',
+	'Name' => 'IE6 Blue Screen',
 	'Description' => "Show's a bluescreen instead the page for IE6 browsers.",
-	'Version' => '1.00',
+	'Version' => '1.01',
 	'Date' => 'Autumn 2011',
 	'Author' => 'IE6BlueScreen',
 	'AuthorUrl' => 'https://github.com/unlight/IE6BlueScreen'
@@ -15,7 +15,7 @@ class IE6BluescreenPlugin implements Gdn_IPlugin {
 		if ($Sender->DeliveryType() == DELIVERY_TYPE_ALL) {
 			$Agent = ArrayValue('HTTP_USER_AGENT', $_SERVER, '');
 			preg_match_all('/MSIE (\d+)/', $Agent, $Match);
-			if (isset($Match[1]) && is_array($Match[1])) {
+			if (isset($Match[1]) && is_array($Match[1]) && count($Match[1]) > 0) {
 				$Version = max($Match[1]);
 				if ($Version == 6) $Sender->AddJsFile('plugins/IE6BlueScreen/IE6Bluescreen/jquery.ie6bluescreen.min.js');
 			}
